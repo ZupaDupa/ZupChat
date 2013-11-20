2def779ea178b4c53dc773401490641c32ba573e
Kalle Rainio <kallerainio@gmail.com>
2013-11-20 23:06:34 +0200

    Cleaned up the server side code.

--- src/server.js
+++ src/server.js
@@ -7,43 +7,30 @@ server.listen(8000);
 
 app.use( express.static(__dirname + '/www') );
 
-/*
-app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-
-app.get('/style.css', function(req, res) {
-  res.sendfile(__dirname + '/style.css');
-});
-
-app.get('/webrtc.io.js', function(req, res) {
-  res.sendfile(__dirname + '/webrtc.io.js');
-});
-
-app.get('/webrtc.io.js', function(req, res) {
-  res.sendfile(__dirname + '/webrtc.io.js');
-});
-
-app.get('/webrtc.io.js', function(req, res) {
-  res.sendfile(__dirname + '/webrtc.io.js');
-});
-*/
 
 
 
 webRTC.rtc.on('connect', function(rtc) {
-  //Client connected
+  console.log('Client '+ rtc.sockets[0].id +' connected');
+});
+
+webRTC.rtc.on('join_room', function(rtc) {
+  console.log('Client joined room '+ rtc.room );
 });
 
 webRTC.rtc.on('send answer', function(rtc) {
-  //answer sent
+  console.log('Answer sent');
 });
 
 webRTC.rtc.on('disconnect', function(rtc) {
-  //Client disconnect 
+  console.log('Client disconnected');
 });
 
+
 webRTC.rtc.on('chat_msg', function(data, socket) {
+  console.log('Chat message');
+  console.log( data );
+
   var roomList = webRTC.rtc.rooms[data.room] || [];
 
   for (var i = 0; i < roomList.length; i++) {
